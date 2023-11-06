a8edab908ce7d7d1b2b7a13ce13f163fff616f0c
OliveiraPauloC <paulo_oliveira2005@hotmail.com>
2023-11-06 16:19:42 -0300

    request validation

--- src/main/java/br/com/paulo/placeservice/domain/PlaceService.java
+++ src/main/java/br/com/paulo/placeservice/domain/PlaceService.java
@@ -1,15 +1,21 @@
 package br.com.paulo.placeservice.domain;
 
+import br.com.paulo.placeservice.api.PlaceRequest;
+import com.github.slugify.Slugify;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
+
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
-    public Mono<Place> create(Place place) {
+    public Mono<Place> create(PlaceRequest placeRequest) {
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.name()), placeRequest.state(), null, null);
         return placeRepository.save(place);
     }
 }
--- src/test/java/br/com/paulo/placeservice/PlaceServiceApplicationTests.java
+++ src/test/java/br/com/paulo/placeservice/PlaceServiceApplicationTests.java
@@ -1,13 +1,50 @@
 package br.com.paulo.placeservice;
 
+import br.com.paulo.placeservice.api.PlaceRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PlaceServiceApplicationTests {
+	@Autowired
+	WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	public void testCreatePlaceSuccess() {
+		var name = "Valid Name";
+		var state = "Valid State";
+		var slug = "valid-name";
+
+		webTestClient
+				.post()
+				.uri("/places")
+				.bodyValue(
+						new PlaceRequest(name, state)
+				)
+				.exchange()
+				.expectBody()
+				.jsonPath("name").isEqualTo(name)
+				.jsonPath("state").isEqualTo(state)
+				.jsonPath("slug").isEqualTo(slug)
+				.jsonPath("createdAt").isNotEmpty()
+				.jsonPath("updatedAt").isNotEmpty();
+	}
+
+	@Test
+	public void testCreatePlaceFailure() {
+		var name = "";
+		var state = "";
+
+		webTestClient
+				.post()
+				.uri("/places")
+				.bodyValue(
+						new PlaceRequest(name, state)
+				)
+				.exchange()
+				.expectStatus().isBadRequest();
 	}
 
 }
